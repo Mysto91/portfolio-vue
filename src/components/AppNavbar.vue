fd4f02d85d66df20a56a713b1e6905772abbec3e
Etienne TRAN <etiennetran@hotmail.fr>
2023-03-11 11:23:33 +0100

    portfolio : start

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav class="px-12 py-5 bg-transparent font-poppins">
+        <div class="h-12 flex justify-between">
+            <a
+                href="/"
+                class="flex items-center justify-center text-lg font-semibold"
+            >
+                Etienne TRAN
+            </a>
+            <ul class="flex items-center justify-center space-x-6">
+                <li>
+                    <NavigationLink url="/about">
+                        About
+                    </NavigationLink>
+                </li>
+                <li>
+                    <NavigationLink url="/">
+                            Contact
+                    </NavigationLink>
+                </li>
+                <li>
+                    <NavigationLink
+                        url="/"
+                        class="px-6 py-4 bg-black text-white rounded-lg"
+                    >
+                        View Work
+                    </NavigationLink>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import NavigationLink from './NavigationLink.vue';
+
+export default defineComponent({
+  name: 'AppNavbar',
+
+  components: {
+    NavigationLink,
+  },
+});
+</script>
---
+++ src/components/NavigationLink.vue
@@ -0,0 +1,23 @@
+<template>
+    <router-link
+        :to="url"
+        class="p-2.5 text-gray-700 text-base font-semibold transition-opacity duration-300 hover:opacity-50"
+    >
+        <slot></slot>
+    </router-link>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NavigationLink',
+
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
