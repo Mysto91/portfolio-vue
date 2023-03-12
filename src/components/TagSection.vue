fd322aa7643f7c8621e111f44b8916bf218cc9ef
Etienne TRAN <etiennetran@hotmail.fr>
2023-03-12 18:54:44 +0100

    about_responsive : start

---
+++ src/components/TagSection.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="flex flex-wrap">
+            <AppTag
+                v-for="(tag, index) in tags"
+                v-bind:key="index"
+                :name="tag"
+            />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AppTag from './AppTag.vue';
+
+export default defineComponent({
+    name: "TagSection",
+
+    components: { 
+        AppTag,
+    },
+
+    setup() {
+        const tags: string[] = [
+            "ui design",
+            "ux-design",
+            "prototyping",
+            "branding",
+            "html/css",
+            "wireframing",
+            "information architecture",
+            "user search",
+            "user interviews",
+            "leadership",
+            "figma",
+            "adobe suite",
+        ];
+        return {
+            tags,
+        };
+    },
+});
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,38 +1,41 @@
 <template>
   <div class="flex flex-col items-center">
-    <div class="w-full flex space-x-12">
-        <img
-          src="images/me.jpg"
-          class="flex-initial w-[330px] rounded-2xl"
-          alt="me"
+    <div class="
+      w-full 
+      flex md:flex-row flex-col items-center
+      md:space-x-12 md:space-y-0 space-y-10"
+    >
+        <AuthorPicture 
+          class="
+            flex-initial 
+            lg:w-[330px] md:w-[300px] sm:w-[425px] w-[430px]"
+        />
+        <div 
+          class="
+            flex-initial 
+            lg:w-[670px] md:w-[580px] 
+            flex flex-col justify-center"
         >
-        <div class="flex-initial w-[670px] flex flex-col justify-center">
-          <h1 class="py-3 text-7xl font-medium leading-[90px]">
-            Hi there !
-          </h1>
-          <p class="text-2xl font-light leading-10">
+          <PageTitle 
+            title="Hi there !"
+            class="
+              lg:text-7xl md:text-6xl sm:text-5xl text-[48px]
+              font-medium
+              lg:leading-[90px] md:leading-[70px] sm:leading-[64px] leading-[48px]"
+          />
+          <p
+            class="
+              sm:text-2xl text-xl
+              font-light 
+              sm:leading-10 leading-8"
+            >
             Fuelled by a passion for designing compelling products, I have a deep desire to excel and continuously improve in my work. Learn more about my journey below.
           </p>
         </div>
-      </div>
+    </div>
     <div class="mt-36 w-full flex space-x-12">
-      <div class="flex-initial w-[600px]">
-        <h1 class="my-5 text-5xl leading-[62px] font-medium">
-          My Career So Far
-        </h1>
-        <p class="pr-14 text-xl leading-8 font-light">
-          Always up for a challenge, I have worked for lean start-ups and was a member of the first New Zealand start-up to attend Y Combinator, the largest start-up accelerator in the world. From there, I worked my way up to Art Director and Team Lead at Appster where I oversaw the design of 30+ mobile and desktop apps. Currently I am a Senior Product Designer at Linktree leading design for the Linktree mobile app
-        </p>
-      </div>
-      <div class="flex-initial w-[440px] flex items-center">
-        <div class="flex flex-wrap">
-          <AppTag
-            v-for="(tag, index) in tags"
-            v-bind:key="index"
-            :name="tag"
-          />
-        </div>
-      </div>
+      <!-- <CareerSection class="flex-initial w-[600px]" /> -->
+      <!-- <TagSection class="flex-initial w-[440px] flex items-center" /> -->
     </div>
     <div>
       Autre section
@@ -41,35 +44,24 @@
 </template>
 
 <script lang="ts">
-import AppTag from '@/components/AppTag.vue';
+import AuthorPicture from '@/components/AuthorPicture.vue';
+import CareerSection from '@/components/CareerSection.vue';
+import PageTitle from '@/components/PageTitle.vue';
+import TagSection from '@/components/TagSection.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'AboutView',
 
   components: {
-    AppTag,
-  },
-
-  setup() {
-    const tags: string[] = [
-      'ui design',
-      'ux-design',
-      'prototyping',
-      'branding',
-      'html/css',
-      'wireframing',
-      'information architecture',
-      'user search',
-      'user interviews',
-      'leadership',
-      'figma',
-      'adobe suite',
-    ];
-
-    return {
-      tags,
-    };
-  },
+    AuthorPicture,
+    TagSection,
+    CareerSection,
+    PageTitle
+},
 });
 </script>
+
+<style scoped>
+
+</style>
