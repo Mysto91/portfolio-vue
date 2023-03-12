d999dea3f2d0a999cf517307571346c1dc80b6b4
Etienne TRAN <etiennetran@hotmail.fr>
2023-03-12 11:22:23 +0100

    Decorative bar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <AppNavbar />
-  <router-view class="mt-20" />
+  <DecorativeBar />
+  <router-view class="mt-1" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import AppNavbar from './components/AppNavbar.vue';
+import DecorativeBar from './components/DecorativeBar.vue';
 
 export default defineComponent({
   name: 'App',
 
   components: {
     AppNavbar,
+    DecorativeBar,
   },
 });
 </script>
