bf18ad8cb6278ca4046e29f60d8de8aa67ba80e1
Etienne TRAN <etiennetran@hotmail.fr>
2023-03-21 22:03:28 +0100

    homepage : animation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
-    <h1>Homepage</h1>
+    <div class="w-full h-[680px] flex items-center justify-center">
+      <div class="relative w-10 animate-spin-slow">
+        <div class="absolute -top-40 -left-16 rounded-full w-40 h-40 bg-red-200 mix-blend-multiply blur-2xl" />
+        <div class="absolute -top-20 left-10 rounded-full w-40 h-40 bg-purple-300 mix-blend-multiply blur-2xl" />
+        <div class="absolute top-0 -left-16 rounded-full w-40 h-40 bg-cyan-200 mix-blend-multiply blur-2xl" />
+        <div class="absolute -top-20 -left-40 rounded-full w-40 h-40 bg-orange-300 mix-blend-multiply blur-2xl" />
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+  @keyframes spin-slow {
+    to {
+      transform: rotate(180deg);
+    }
+  }
+
+  .animate-spin-slow {
+    animation: spin-slow 15s linear infinite alternate;
+  }
+</style>
