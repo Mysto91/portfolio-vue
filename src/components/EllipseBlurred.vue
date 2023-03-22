25bf94a102fbfd5f334d662fc9b6e67416b09064
Etienne TRAN <etiennetran@hotmail.fr>
2023-03-22 08:02:03 +0100

    homepage : refacto ellipse blurred

---
+++ src/components/EllipseBlurred.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="rounded-full mix-blend-multiply blur-2xl" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'EllipseBlurred',
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,45 @@
   <div>
     <div class="w-full h-[680px] flex items-center justify-center">
       <div class="relative w-10 animate-spin-slow">
-        <div class="absolute -top-40 -left-16 rounded-full w-40 h-40 bg-red-200 mix-blend-multiply blur-2xl" />
-        <div class="absolute -top-20 left-10 rounded-full w-40 h-40 bg-purple-300 mix-blend-multiply blur-2xl" />
-        <div class="absolute top-0 -left-16 rounded-full w-40 h-40 bg-cyan-200 mix-blend-multiply blur-2xl" />
-        <div class="absolute -top-20 -left-40 rounded-full w-40 h-40 bg-orange-300 mix-blend-multiply blur-2xl" />
+        <EllipseBlurred
+          class="absolute -top-40 -left-16 bg-red-200"
+          :class="[sizeClasses]"
+        />
+        <EllipseBlurred
+          class="absolute -top-20 left-10 bg-purple-300"
+          :class="[sizeClasses]"
+        />
+        <EllipseBlurred
+          class="absolute top-0 -left-16 bg-cyan-200"
+          :class="[sizeClasses]"
+        />
+        <EllipseBlurred
+          class="absolute -top-20 -left-40 bg-orange-300"
+          :class="[sizeClasses]"
+        />
       </div>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import EllipseBlurred from '@/components/EllipseBlurred.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    EllipseBlurred,
+  },
+
+  setup() {
+    return {
+      sizeClasses: 'w-40 h-40 md:w-48 md:h-48 lg:w-72 lg:h-72',
+    };
+  },
+});
+</script>
 
 <style scoped>
   @keyframes spin-slow {
