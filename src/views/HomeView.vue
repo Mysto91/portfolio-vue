13215413667c005a0d2b432fe24afd7e427f5910
Etienne TRAN <etiennetran@hotmail.fr>
2023-04-09 21:07:16 +0200

    homepage : contact section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,6 +10,7 @@
     <div class="absolute top-0 pr-7">
       <HomepageHeader class="animate-slide-up" />
       <WorkList class="mt-20 sm:mt-40 animate-slide-up" />
+      <ContactForm class="mt-20 mb-32" />
     </div>
   </div>
 </template>
@@ -19,11 +20,13 @@ import { defineComponent } from 'vue';
 import EllipseAnimated from '@/components/EllipseAnimated.vue';
 import HomepageHeader from '@/components/homepage/HomepageHeader.vue';
 import WorkList from '@/components/homepage/WorkList.vue';
+import ContactForm from '@/components/ContactForm.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
+    ContactForm,
     WorkList,
     HomepageHeader,
     EllipseAnimated,
