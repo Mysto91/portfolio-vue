921fb282006a586e6056413ad53332586aa38a0c
Etienne TRAN <etiennetran@hotmail.fr>
2023-04-10 12:33:26 +0200

    footer : début

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <AppNavbar />
   <DecorativeBar />
-  <router-view class="mx-auto px-7 max-w-[1140px]" />
+  <router-view class="mx-auto px-7 max-w-[1140px] h-[3000px]" />
+  <AppFooter />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import AppNavbar from './components/AppNavbar.vue';
 import DecorativeBar from './components/DecorativeBar.vue';
+import AppFooter from '@/components/Footer.vue';
 
 export default defineComponent({
   name: 'App',
 
   components: {
+    AppFooter,
     AppNavbar,
     DecorativeBar,
   },
