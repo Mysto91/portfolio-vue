9ca120350124cb247a818dfaef2c2543571b0cb3
Etienne <etienne.tran@bimpli.com>
2023-11-23 15:44:18 +0100

    Fix rails icon issue

--- src/components/icons/IconRails.vue
+++ src/components/icons/IconRails.vue
@@ -1,8 +1,10 @@
 <template>
   <svg
-    viewBox="0 -166 512 512"
-     preserveAspectRatio="xMidYMid"
-     fill="#000000"
+      height="30"
+      width="30"
+      viewBox="0 -166 512 512"
+      preserveAspectRatio="xMidYMid"
+      fill="#000000"
   >
     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
