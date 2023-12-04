3f857a10a4bd8af062a6a14253a40c4e6eaf9ea0
Etienne Tran <62669050+Mysto91@users.noreply.github.com>
2023-12-04 16:40:49 +0100

    pf-36 : redesign cart item (#43)
    Co-authored-by: Etienne <etienne.tran@bimpli.com>

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,69 +1,47 @@
 <template>
   <router-link
     :to="url"
-    :style="imageStyle"
     class="
+    relative
     flex flex-col justify-end
     w-full h-[300px] lg:h-[400px]
-    rounded-2xl
-    drop-shadow-2xl
-    bg-auto bg-cover
-    ease-in-out duration-300
-    group hover:scale-[102%]"
+    p-8
+    border border-gray-400 rounded-2xl
+    bg-primary hover:shadow-2xl
+    text-background
+    transition ease-in-out duration-300"
   >
-    <slot name="image"></slot>
-    <div class="
-      relative
-      h-24
-      p-5 sm:px-7 sm:pb-14
-      text-white
-      rounded-b-2xl
-      ease-in-out duration-300
-      group-hover:h-full
-    ">
-      <div class="
-        absolute
-        inset-0
-        bg-black overlay
-        rounded-b-2xl
-        ease-in-out duration-300
-        group-hover:bg-black"
-      ></div>
-      <div class="relative z-10 h-full flex flex-col">
-        <h4 class="
-          flex-none
-          text-xl sm:text-2xl md:text-3xl
-          sm:leading-7"
-        >
-          <slot name="title"></slot>
-        </h4>
-        <div class="
-          flex-grow
-          flex flex-col justify-center
-          opacity-0
-          ease-in-out duration-300
-          group-hover:opacity-100"
-        >
-          <p class="
-            flex-grow
-            flex items-center justify-center
-            text-sm sm:text-base md:text-lg"
-          >
-            <slot name="description"></slot>
-          </p>
-          <div>
-            <slot name="technologies"></slot>
-          </div>
-        </div>
-      </div>
+    <div>
+      <slot name="header"></slot>
+    </div>
+
+    <h4 class="
+      mt-4
+      flex-none
+      text-xl sm:text-2xl md:text-3xl
+      font-semibold
+      sm:leading-7"
+    >
+      <slot name="title"></slot>
+    </h4>
+
+    <p class="
+      mt-2
+      flex-grow
+      text-sm sm:text-base md:text-lg
+      text-gray-300"
+    >
+      <slot name="description"></slot>
+    </p>
+
+    <div class="mt-2">
+      <slot name="technologies"></slot>
     </div>
   </router-link>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
-import { Url } from '@/types/request';
-import { useImageStyle } from '@/composables/useStyle';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'CardItem',
@@ -73,34 +51,6 @@ export default defineComponent({
       type: String,
       required: true,
     },
-    backgroundImageUrl: {
-      type: String as PropType<Url | null>,
-      default: null,
-    },
-  },
-
-  setup(props) {
-    const { imageStyle } = useImageStyle({
-      imageUrl: props.backgroundImageUrl,
-      defaultImageUrl: 'https://www.sapphirewebsolutions.com/wp-content/uploads/2019/09/Web-Development-Trends.jpg',
-    });
-
-    return {
-      imageStyle,
-    };
   },
 });
 </script>
-
-<style scoped>
-  .overlay {
-    -webkit-mask-image: -webkit-gradient(linear, left bottom, left top, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
-  }
-
-  .bg-image {
-     background-image: linear-gradient(
-        180deg,
-        rgba(16,32,44,0) 62%,#3db4cf),
-        url("https://www.sapphirewebsolutions.com/wp-content/uploads/2019/09/Web-Development-Trends.jpg")
-  }
-</style>
