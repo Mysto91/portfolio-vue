0169ab002701bb61cfa67f27ab26e74690fe8a83
Etienne <etienne.tran@bimpli.com>
2023-12-13 16:26:54 +0100

    Revert "pf-59 : add authentication header to api calls (#60)"
    This reverts commit b4ad3e7360bcdb4511fe4e7e76e5c9e0f49eb3b9.

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,28 +1,6 @@
 import axios from 'axios';
-import { useAuthStore } from '@/stores/useAuthStore';
-import { login } from '@/api/authApi';
-import { storeToRefs } from 'pinia';
 
 // eslint-disable-next-line
 export const apiClient = axios.create({
   baseURL: `${process.env.VUE_APP_API_URL}/api`,
 });
-
-apiClient.interceptors.request.use(
-  async (config) => {
-    const authStore = useAuthStore();
-    const { tokenIsExpired, token } = storeToRefs(authStore);
-
-    if (!token.value || tokenIsExpired.value) {
-      authStore.setToken(await login());
-    }
-
-    if (token.value) {
-      // eslint-disable-next-line no-param-reassign
-      config.headers.Authorization = `Bearer ${token.value}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
--- src/api/authClient.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-// eslint-disable-next-line
-export const authClient = axios.create({
-  baseURL: `${process.env.VUE_APP_API_URL}/auth`,
-});
