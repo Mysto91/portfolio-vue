f45044427333f1819444337187b1671c8119e7fa
Etienne <etienne.tran@bimpli.com>
2023-12-18 11:08:46 +0100

    pf-38 : add social network on homepage header

---
+++ src/components/icons/IconMail.vue
@@ -0,0 +1,40 @@
+<template>
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    height="20"
+    width="20"
+  >
+    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <path
+        d="M4 7.00005L10.2 11.65C11.2667 12.45 12.7333 12.45 13.8 11.65L20 7"
+        class="stroke-current"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+      </path>
+      <rect
+        class="stroke-current"
+        x="3"
+        y="5"
+        width="18"
+        height="14"
+        rx="2"
+        stroke-width="2"
+        stroke-linecap="round"
+      >
+      </rect>
+    </g>
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IconMail',
+});
+</script>
--- src/constants/socialNetwork.ts
+++ src/constants/socialNetwork.ts
@@ -1,4 +1,7 @@
-export const FACEBOOK_URL = 'https://www.facebook.com/etienne.tran.77';
-export const GITHUB_URL = 'https://github.com/Mysto91';
-export const INSTAGRAM_URL = 'https://www.instagram.com/etienne_sushi91/';
-export const LINKEDIN_URL = 'https://www.linkedin.com/in/etienne-tran';
+// eslint-disable-next-line import/prefer-default-export
+export enum SocialNetworkUrl {
+  FACEBOOK = 'https://www.facebook.com/etienne.tran.77',
+  GITHUB = 'https://github.com/Mysto91',
+  INSTAGRAM = 'https://www.instagram.com/etienne_sushi91/',
+  LINKEDIN = 'https://www.linkedin.com/in/etienne-tran',
+}
