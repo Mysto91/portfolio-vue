00eaf22df8fb74f5ebb045997d272e3cd1c69795
Etienne Tran <62669050+Mysto91@users.noreply.github.com>
2024-08-20 19:04:03 +0200

    pf-95 : En tant qu'utilisateur, je dois pouvoir contacter le propriétaire du portfolio (#97)

---
+++ src/components/AppErrorMessage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-1 text-red-400 text-xs">
+    {{ message }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+});
+</script>
